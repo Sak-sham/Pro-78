dba0797708d7d164ae51caabb1b497c2c3f53274
Saksham <67902670+Sak-sham@users.noreply.github.com>
2023-04-12 16:23:07 +0100

    Add files via upload

---
+++ screens/StarMap.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler';
+import WebView from 'react-native-webview';
+
+export default class StarMapScreen extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            starmap: {}
+        }
+    }
+
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+                <Text>
+                    Star Map
+                </Text>
+            </View>
+        )
+    }
+
+    <View style={styles.mapContainer}>
+    <WebView
+        style={styles.map}
+        region={{
+            latitude: this.state.location.latitude,
+            longitude: this.state.location.longitude,
+        }}
+    ></WebView> </View>
+
+    <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        latitude='28.704060'
+        latitudeTextColor="#ffff"
+        onChangeText={(text) => {
+            this.setState({
+                latitude: 28.704060
+            })
+        }}
+    />
+
+    <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        longitude='77.102493'
+        longitudeTextColor="#ffff"
+        onChangeText={(text) => {
+            this.setState({
+                longitude: 77.102493
+            })
+        }}
+    />
+
+    ﻿
+
+    const {longitude, latitude } = this.state;
+    const path= https://virtualsky.lco.global/embed/index.html?longitude=
+    ${longitude}&latitude=${latitude} & constellations=true&constellationlabels=
+    true&showstarlabels=true&gridlines_az=true&live=true&projection=stereo&show
+    date=false&showposition=false
+
+
